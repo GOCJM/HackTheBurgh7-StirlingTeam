99d731dd39d70ad931b0dde5da0e9def63c89e80
adamSH91 <adamserraj91@gmail.com>
2021-03-06 19:11:11 +0100

    Updated the title on index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="section text-center">
-        <h2 class="title">Your main section here</h2>
+        <h2 class="title">Simplify your experience on the web during the pandemic!</h2>
     </div>
 @stop
